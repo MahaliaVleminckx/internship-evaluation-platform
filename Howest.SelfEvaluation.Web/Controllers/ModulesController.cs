1de8e812cd8a4d17469cf91890f17c0b032ac74a
MahaliaVleminckx <mahalia.vleminckx@student.howest.be>
2026-03-20 17:50:31 +0100

    Add Modules page

---
+++ Howest.SelfEvaluation.Web/Controllers/ModulesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Howest.SelfEvaluation.Web.Controllers
+{
+    public class ModulesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
