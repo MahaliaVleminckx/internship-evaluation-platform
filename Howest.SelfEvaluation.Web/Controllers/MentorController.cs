8b2525a56ffb894487778ce5195678d7353dffcd
JasonDejonckheere <jason.dejonckheere@student.howest.be>
2026-03-23 05:25:00 +0100

    Add default mentor controller

---
+++ Howest.SelfEvaluation.Web/Controllers/MentorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Howest.SelfEvaluation.Web.Controllers
+{
+    public class MentorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
