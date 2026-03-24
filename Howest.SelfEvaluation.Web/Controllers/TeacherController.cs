fdcac3a7bc78893cbdf0722079ee7ad101cc74fa
JasonDejonckheere <jason.dejonckheere@student.howest.be>
2026-03-24 02:12:46 +0100

    Add default TeacherController

---
+++ Howest.SelfEvaluation.Web/Controllers/TeacherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Howest.SelfEvaluation.Web.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
