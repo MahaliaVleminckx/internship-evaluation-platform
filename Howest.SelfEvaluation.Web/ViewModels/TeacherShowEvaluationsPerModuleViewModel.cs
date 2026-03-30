b99f5c22709bbd492eb603a931e875860a4c7d54
MahaliaVleminckx <mahalia.vleminckx@student.howest.be>
2026-03-30 17:46:47 +0200

    Move Index to ShowEvaluations and show Modules

---
+++ Howest.SelfEvaluation.Web/ViewModels/TeacherShowEvaluationsPerModuleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Howest.SelfEvaluation.Core.Entities;
+using System;
+
+namespace Howest.SelfEvaluation.Web.ViewModels
+{
+    public class TeacherShowEvaluationsPerModuleViewModel
+    {
+        public Guid ModuleId { get; set; }
+        public string ModuleName { get; set; }
+        public string ModuleDescription { get; set; }
+        public List<Evaluation> Evaluations { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
