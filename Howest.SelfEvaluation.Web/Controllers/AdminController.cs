3595dea68238fce71f3a8b192f808d05814d2540
MahaliaVleminckx <mahalia.vleminckx@student.howest.be>
2026-03-31 16:58:33 +0200

    Add base layout for Create Module page

---
+++ Howest.SelfEvaluation.Web/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Howest.SelfEvaluation.Core.Entities;
+using Howest.SelfEvaluation.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+//using System.Reflection;
+
+namespace Howest.SelfEvaluation.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly SelfEvaluationsContext _db;
+
+        public AdminController (SelfEvaluationsContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+
+        public IActionResult CreateModule()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateModule(string name, string description, Guid? ownerId)
+        {
+            var module = new Module
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Description = description,
+                OwnerId = ownerId,
+                Created = DateTime.Now
+            };
+
+            _db.Modules.Add(module);
+            _db.SaveChanges();
+            return RedirectToAction("CreateModule");
+        }
+    }
+}
