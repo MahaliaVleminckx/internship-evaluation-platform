527868420c99d73a9381311868b135bec7a6cbc7
JasonDejonckheere <jason.dejonckheere@student.howest.be>
2026-05-01 04:33:01 +0200

    Update entity ApplicationUser with first and lastname #5

--- Howest.SelfEvaluation.Core/Entities/ApplicationUser.cs
+++ Howest.SelfEvaluation.Core/Entities/ApplicationUser.cs
@@ -10,13 +10,12 @@ namespace Howest.SelfEvaluation.Core.Entities
 {
     public class ApplicationUser : BaseEntity
     {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string Username { get; set; }
         public ICollection<Module> OwnerModules { get; set; }
         public ICollection<Module> Modules { get; set; }
         public ICollection<EvaluationScore> StudentEvaluationScores { get; set; }
         public string Role { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime? Updated { get; set; }
-        public DateTime? Deleted { get; set; }
     }
 }
