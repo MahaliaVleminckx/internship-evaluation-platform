60c9060e5f23a1627957ae61d7922d1b615de347
MahaliaVleminckx <mahalia.vleminckx@student.howest.be>
2026-05-17 15:50:35 +0200

    Fix manual labels in EditUser view #13

--- Howest.SelfEvaluation.Web/ViewModels/Admin/AdminEditUsersViewModel.cs
+++ Howest.SelfEvaluation.Web/ViewModels/Admin/AdminEditUsersViewModel.cs
@@ -1,11 +1,17 @@
-﻿namespace Howest.SelfEvaluation.Web.ViewModels.Admin
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Howest.SelfEvaluation.Web.ViewModels.Admin
 {
     public class AdminEditUsersViewModel
     {
         public Guid Id { get; set; }
+        [Display(Name = "Voornaam")]
         public string Firstname { get; set; }
+        [Display(Name = "Achternaam")]
         public string Lastname { get; set; }
+        [Display(Name = "E-mail")]
         public string Username { get; set; }
+        [Display(Name = "Rol")]
         public string Role { get; set; }
     }
 }
