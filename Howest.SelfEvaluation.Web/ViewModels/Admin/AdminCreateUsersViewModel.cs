90fb44e509a77c14040cbfeec09b559b0e1213a8
MahaliaVleminckx <mahalia.vleminckx@student.howest.be>
2026-05-17 15:46:52 +0200

    Fix manual labels in CreateUser view #13

--- Howest.SelfEvaluation.Web/ViewModels/Admin/AdminCreateUsersViewModel.cs
+++ Howest.SelfEvaluation.Web/ViewModels/Admin/AdminCreateUsersViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace Howest.SelfEvaluation.Web.ViewModels.Admin
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Howest.SelfEvaluation.Web.ViewModels.Admin
 {
     public class AdminCreateUsersViewModel
     {
+        [Display(Name ="Voornaam")]
         public string Firstname { get; set; } = default!;
+        [Display(Name = "Achternaam")]
         public string Lastname { get; set; } = default!;
+        [Display(Name = "E-mail")]
         public string Username { get; set; } = default!;
+        [Display(Name = "Rol")]
         public string Role { get; set; } = default!;
     }
 }
